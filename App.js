40a8d883dc707522bb9a7f0d536be335734594cb
ramieeee <rambotty@gmail.com>
2022-09-25 13:24:35 +0900

    Fix errors while loading RN

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
 import React from 'react';
-import {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { StyleSheet, Text, useColorScheme, View } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+//pages
+import LoadingScreen from './screens/LoadingScreen';
 
 import {
   Colors,
@@ -29,26 +19,30 @@ const styles = StyleSheet.create({
   view: {
     flex: 1,
     flexDirection: 'column',
+    backgroundColor: '#191F2D',
   },
   textHead: {
     fontSize: 18,
     alignItems: 'center',
     justifyContent: 'center',
-    width: "100%",
-    textAlign: "center",
-    top:100,
-  }
-  
+    width: '100%',
+    textAlign: 'center',
+    top: 100,
+    color: 'white',
+  },
 });
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
-
   return (
-    <View style={styles.view}>
-      <Text style={styles.textHead}>Set number of Players</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="hello" component={LoadingScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
