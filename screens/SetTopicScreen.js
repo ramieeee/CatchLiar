8ea2b5ca672d11cd0f61f6f1314589b96c363e32
ramieeee <rambotty@gmail.com>
2022-10-08 23:58:19 +0900

    Apply FlatList to show list of topics

--- screens/SetTopicScreen.js
+++ screens/SetTopicScreen.js
@@ -5,86 +5,99 @@ import {
   View,
   TouchableHighlight,
   Pressable,
+  ScrollView,
+  FlatList,
+  SafeAreaView,
 } from 'react-native';
-import { Picker } from '@react-native-picker/picker';
 
 // Icons
 import HouseIcon from '../assets/HouseIcon';
 import GoBackIcon from '../assets/GoBackIcon';
 
 // topic data list
-import topicList from '../jsonData/topicList';
+import topicList from '../jsonData/topicList.json';
 
+// redux
+import { useSelector, useDispatch } from 'react-redux';
+
+const Item = ({ title, id }) => (
+  <TouchableHighlight
+    onPress={() => console.log(id)}
+    underlayColor={styles.btnsOnPress}
+    style={styles.topicBtn}>
+    <Text style={styles.plainText}>{title}</Text>
+  </TouchableHighlight>
+);
+
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: '#20262B',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%',
+  },
+  containerUpp: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '10%',
+    width: '100%',
+  },
+  containerTop: {
+    height: '10%',
+    width: '90%',
+    paddingTop: 50,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  containerMid: {
+    flex: 1,
+    flexDirection: 'row',
+    height: '80%',
+  },
+
+  textHeader: {
+    color: 'white',
+    fontSize: 24,
+  },
+  plainText: {
+    color: 'white',
+    fontSize: 18,
+  },
+
+  textContainter: {
+    width: 90,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  text1: {
+    color: 'white',
+    fontSize: 18,
+  },
+  text2: {
+    color: 'red',
+    fontSize: 18,
+  },
+  btnsOnPress: '#88AEB6',
+  topicBtn: {
+    marginVertical: 20,
+    marginHorizontal: 20,
+    backgroundColor: '#42595F',
+    width: 140,
+    height: 80,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 export default function SetLiarsScreen({ navigation }) {
-  const styles = StyleSheet.create({
-    background: {
-      backgroundColor: '#20262B',
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: '100%',
-    },
-    containerUpp: {
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: '10%',
-      width: '100%',
-    },
-    containerTop: {
-      height: '10%',
-      width: '90%',
-      paddingTop: 50,
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-    },
-    containerMid: {
-      height: '60%',
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center',
-      // backgroundColor: 'pink',
-    },
+  const test = useSelector(store => store.playInfo);
 
-    bottomBtnContainter: {
-      height: '20%',
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingBottom: 70,
-    },
+  const renderItem = ({ item }) => (
+    <Item title={item.topic} id={item.id} style={{ width: '45%' }} />
+  );
 
-    textHeader: {
-      color: 'white',
-      fontSize: 24,
-    },
-    plainText: {
-      color: 'white',
-      fontSize: 18,
-    },
-    textContainter: {
-      width: 90,
-      flexDirection: 'row',
-      alignItems: 'center',
-      // backgroundColor: 'pink',
-      justifyContent: 'space-between',
-    },
-    text1: {
-      color: 'white',
-      fontSize: 18,
-    },
-    text2: {
-      color: 'red',
-      fontSize: 18,
-    },
-    btnsOnPress: '#88AEB6',
-    bottomBtn: {
-      backgroundColor: '#42595F',
-      width: 290,
-      height: 46,
-      borderRadius: 10,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
   return (
     <View style={styles.background}>
       <View style={styles.containerTop}>
@@ -99,19 +112,19 @@ export default function SetLiarsScreen({ navigation }) {
           <HouseIcon />
         </Pressable>
       </View>
+
       <View style={styles.containerUpp}>
         <Text style={styles.textHeader}>Choose Topic</Text>
       </View>
-
-      <View style={styles.containerMid}></View>
-
-      <View style={styles.bottomBtnContainter}>
-        <TouchableHighlight
-          onPress={() => console.log('topic')}
-          underlayColor={styles.btnsOnPress}
-          style={styles.bottomBtn}>
-          <Text style={styles.plainText}>Next</Text>
-        </TouchableHighlight>
+      <View style={styles.containerMid}>
+        <SafeAreaView style={{ flex: 1, alignItems: 'center' }}>
+          <FlatList
+            data={topicList}
+            renderItem={renderItem}
+            numColumns={2}
+            keyExtractor={item => item.id}
+          />
+        </SafeAreaView>
       </View>
     </View>
   );
